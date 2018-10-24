e21822384e64924d062e86bad481d1a1e7031ba2
Edas Lakavicius <elakavicius@gmail.com>
2018-10-24 20:23:36 +0300

    Register form changed

--- FaceRecognitionApp/RegisterCamera.Designer.cs
+++ FaceRecognitionApp/RegisterCamera.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.CameraPictureBox = new System.Windows.Forms.PictureBox();
             this.SubmitButton = new System.Windows.Forms.Button();
+            this.RegisterCameraRegisterButton = new System.Windows.Forms.Button();
+            this.RegisterCameraRetakeButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.CameraPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // CameraPictureBox
             // 
             this.CameraPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.CameraPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CameraPictureBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.CameraPictureBox.Location = new System.Drawing.Point(0, 0);
             this.CameraPictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CameraPictureBox.Name = "CameraPictureBox";
@@ -47,6 +49,7 @@
             // SubmitButton
             // 
             this.SubmitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(108)))), ((int)(((byte)(108)))));
+            this.SubmitButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SubmitButton.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.SubmitButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.SubmitButton.FlatAppearance.BorderSize = 0;
@@ -64,6 +67,48 @@
             this.SubmitButton.UseVisualStyleBackColor = false;
             this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
             // 
+            // RegisterCameraRegisterButton
+            // 
+            this.RegisterCameraRegisterButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RegisterCameraRegisterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(108)))), ((int)(((byte)(108)))));
+            this.RegisterCameraRegisterButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RegisterCameraRegisterButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.RegisterCameraRegisterButton.FlatAppearance.BorderSize = 0;
+            this.RegisterCameraRegisterButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RegisterCameraRegisterButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RegisterCameraRegisterButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegisterCameraRegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.RegisterCameraRegisterButton.Location = new System.Drawing.Point(443, 390);
+            this.RegisterCameraRegisterButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RegisterCameraRegisterButton.Name = "RegisterCameraRegisterButton";
+            this.RegisterCameraRegisterButton.Size = new System.Drawing.Size(441, 39);
+            this.RegisterCameraRegisterButton.TabIndex = 2;
+            this.RegisterCameraRegisterButton.Tag = "photo_button";
+            this.RegisterCameraRegisterButton.Text = "Register";
+            this.RegisterCameraRegisterButton.UseVisualStyleBackColor = false;
+            this.RegisterCameraRegisterButton.Click += new System.EventHandler(this.RegisterCameraRegisterButton_Click);
+            // 
+            // RegisterCameraRetakeButton
+            // 
+            this.RegisterCameraRetakeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RegisterCameraRetakeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(108)))), ((int)(((byte)(108)))));
+            this.RegisterCameraRetakeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RegisterCameraRetakeButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.RegisterCameraRetakeButton.FlatAppearance.BorderSize = 0;
+            this.RegisterCameraRetakeButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RegisterCameraRetakeButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RegisterCameraRetakeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegisterCameraRetakeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.RegisterCameraRetakeButton.Location = new System.Drawing.Point(0, 390);
+            this.RegisterCameraRetakeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RegisterCameraRetakeButton.Name = "RegisterCameraRetakeButton";
+            this.RegisterCameraRetakeButton.Size = new System.Drawing.Size(445, 39);
+            this.RegisterCameraRetakeButton.TabIndex = 3;
+            this.RegisterCameraRetakeButton.Tag = "photo_button";
+            this.RegisterCameraRetakeButton.Text = "Retake a photo";
+            this.RegisterCameraRetakeButton.UseVisualStyleBackColor = false;
+            this.RegisterCameraRetakeButton.Click += new System.EventHandler(this.RegisterCameraRetakeButton_Click);
+            // 
             // RegisterCamera
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -71,8 +116,10 @@
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(884, 429);
             this.ControlBox = false;
-            this.Controls.Add(this.CameraPictureBox);
             this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.RegisterCameraRetakeButton);
+            this.Controls.Add(this.RegisterCameraRegisterButton);
+            this.Controls.Add(this.CameraPictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
@@ -81,9 +128,9 @@
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "fRiEndcognition";
-            this.Shown += new System.EventHandler(this.RegisterCamera_Shown);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RegisterCamera_Closing);
             this.Load += new System.EventHandler(this.RegisterCamera_Load);
+            this.Shown += new System.EventHandler(this.RegisterCamera_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.CameraPictureBox)).EndInit();
             this.ResumeLayout(false);
 
@@ -92,6 +139,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox CameraPictureBox;
+        private System.Windows.Forms.Button RegisterCameraRegisterButton;
+        private System.Windows.Forms.Button RegisterCameraRetakeButton;
         private System.Windows.Forms.Button SubmitButton;
     }
 }
